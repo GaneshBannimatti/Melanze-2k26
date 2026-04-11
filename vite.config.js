3f588df45c827ce32c02bf7efd7ff33fa8d0aa01
Admin <ganeshbannimatti36@gmail.com>
2026-04-11 09:52:38 +0530

    first commit....

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/melanze-2k26/",   // ✅ safest for Vercel
+  base: "/",   // ✅ CORRECT for Vercel
 })
